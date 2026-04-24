33c1ba225347e063e076032c64492361d71bc00d
ezzowali <zwali.eng@gmail.com>
2026-04-24 13:38:06 +0300

    fix db issue

--- util/database.js
+++ util/database.js
@@ -1,24 +1,27 @@
-const Sequelize=require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect:'mssql',
-        host:'sqldatabasecrochet.database.windows.net',
-        port:1433,
-        dialectOptions:{
-            options:{
-                encrypt:true,
-                trustServerCertificate:false
-            }
-        }
-    }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: "postgres",
+        logging: false,
+      }
+    );
 
-sequelize.authenticate()
-  .then(() => console.log('Connected successfully'))
-  .catch(err => console.error('Connection failed:', err));
-
-    module.exports=sequelize;
+module.exports = sequelize;
